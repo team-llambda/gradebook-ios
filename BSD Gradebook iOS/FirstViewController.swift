2702e0fa3f25845ece6a2f2120a89da619acbaaf
Alan Chu <ualch9@gmail.com>
2019-01-28 19:08:20 -0800

    Initial Commit

---
+++ BSD Gradebook iOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BSD Gradebook iOS
+//
+//  Created by Alan Chu on 1/28/19.
+//  Copyright © 2019 Aeta. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
